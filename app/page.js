be176900082dd1aa0d0716fa81a300e50228333a
Łukasz Wnęk <lukasz@northpass.com>
2023-07-25 11:06:36 +0200

    Fetch courses from NP

--- app/page.js
+++ app/page.js
@@ -1,10 +1,16 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { getCourses } from "../lib/northpass";
 
 export default async function Index() {
   const supabase = createServerComponentClient({ cookies });
 
-  const { data: users } = await supabase.from("users").select("id");
+  const { data: users } = await supabase.from("users").select("api_key");
+  const { api_key: apiKey } = users[0];
+
+  const courses = await getCourses(apiKey);
+
+  console.log(courses)
 
   return (
     <ul className="my-auto">
