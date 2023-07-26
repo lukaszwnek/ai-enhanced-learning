584cd111c180fba2b9e4fd99577542a2e5c6774a
Dorian Janiak <dorianjaniak1997@gmail.com>
2023-07-26 11:44:33 +0200

    Style Search Results and skip id in prompt

--- app/search-courses/page.js
+++ app/search-courses/page.js
@@ -12,20 +12,41 @@ export default async function Page({ searchParams }) {
     const searchResults = await searchCourses({ supabase, embedding });
     const results =
       searchResults.length > 0 ? await getResults({ term, searchResults }) : [];
-    return results;
+    return results.map((result) => {
+      return {
+        ...result,
+        image_url: searchResults.find(e =>
+          e.id == result.id
+        ).image_url
+      }
+    });
   }
 
   if (searchParams.term) {
     const results = await search(searchParams.term);
     return (
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>
-            {result.name}, relevance: {result.relevance}
-          </li>
-        ))}
-      </ul>
-    );
+			<div>
+				<div className="page-header">
+					<div className="title">Search Results</div>
+					<a href="/" className="btn btn-blue">
+						Go Back
+					</a>
+				</div>
+				<ul className="courses-row">
+					{results.map((result, index) => (
+						<li key={index} className="course-card">
+							<img src={result.image_url} />
+							<div className="course-content">
+								<div className="course-title">{result.name}</div>
+								<div className="course-revelance">
+									Relevance: {result.relevance}
+								</div>
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
+		);
   } else {
     return (
       <form>
