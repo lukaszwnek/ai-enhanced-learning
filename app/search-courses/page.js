02e485cfbf94debd39f4c74a530cea07b300f253
Łukasz Wnęk <lukasz@northpass.com>
2023-07-26 12:03:12 +0200

    Refactor the mechanism for getting search results

--- app/search-courses/page.js
+++ app/search-courses/page.js
@@ -10,43 +10,32 @@ export default async function Page({ searchParams }) {
     const supabase = createServerComponentClient({ cookies });
     const embedding = await getEmbeddingForTerm(term);
     const searchResults = await searchCourses({ supabase, embedding });
-    const results =
-      searchResults.length > 0 ? await getResults({ term, searchResults }) : [];
-    return results.map((result) => {
-      return {
-        ...result,
-        image_url: searchResults.find(e =>
-          e.id == result.id
-        ).image_url
-      }
-    });
+    const results = await getResults({ term, searchResults });
+    return results;
   }
 
   if (searchParams.term) {
     const results = await search(searchParams.term);
     return (
-			<div className="w-full">
-				<div className="page-header">
-					<div className="title">Search Results</div>
-					<a href="/" className="btn btn-blue">
-						Go Back
-					</a>
-				</div>
-				<ul className="courses-row">
-					{results.map((result, index) => (
-						<li key={index} className="course-card">
-							<img src={result.image_url} />
-							<div className="course-content">
-								<div className="course-title">{result.name}</div>
-								<div className="course-revelance">
-									Relevance: {result.relevance}
-								</div>
-							</div>
-						</li>
-					))}
-				</ul>
-			</div>
-		);
+      <div className="w-full">
+        <div className="page-header">
+          <div className="title">Search Results</div>
+          <a href="/search-courses" className="btn btn-blue">
+            Go Back
+          </a>
+        </div>
+        <ul className="courses-row">
+          {results.map((result, index) => (
+            <li key={index} className="course-card">
+              <img src={result.image_url} />
+              <div className="course-content">
+                <div className="course-title">{result.name}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   } else {
     return (
       <form>
