09854f970649ba27c81bf93391221c862baad21c
Dorian Janiak <dorianjaniak1997@gmail.com>
2023-07-26 12:21:43 +0200

    Adjust homepage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,15 @@
 export default async function Index() {
   return (
-    <div>
-      <a href="/index-courses" className="btn btn-blue mr-4">Index Courses</a>
-      <a href="/search-courses" className="btn btn-blue">Search Courses</a>
-    </div>
-  );
+		<div>
+			<div className="homepage-title">AI Enhanced Learning</div>
+			<div className="homepage-buttons-container">
+				<a href="/index-courses" className="btn btn-blue mr-4">
+					Index Courses
+				</a>
+				<a href="/search-courses" className="btn btn-blue">
+					Search Courses
+				</a>
+			</div>
+		</div>
+	);
 }
